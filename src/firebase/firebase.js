df0937fe79b6210a1825f70a7ed857bc989389af
Max Garceau <maxwellgarceau@gmail.com>
2018-05-25 20:46:25 -0500

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,32 +1,34 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyBwRVJvzyRsF14BZo5PaPrfmZdmtTQOL4U',
-  authDomain: 'expensify-bd648.firebaseapp.com',
-  databaseURL: 'https://expensify-bd648.firebaseio.com',
-  projectId: 'expensify-bd648',
-  storageBucket: 'expensify-bd648.appspot.com',
-  messagingSenderId: '934401696338'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-// child_removed
-database.ref('expenses').on('child_removed', (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
+export { firebase, database as default };
 
-// child_changed
-database.ref('expenses').on('child_changed', (snapshot) => {
-  console.log('Snapshot Key: ', snapshot.key, ' Value: ', snapshot.val());
-});
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-// child_added
-database.ref('expenses').on('child_added', (snapshot) => {
-  console.log('Snapshot Key: ', snapshot.key, ' Value: ', snapshot.val());
-});
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log('Snapshot Key: ', snapshot.key, ' Value: ', snapshot.val());
+// });
+
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log('Snapshot Key: ', snapshot.key, ' Value: ', snapshot.val());
+// });
 
 // database.ref('expenses')
 //   .once('value')
