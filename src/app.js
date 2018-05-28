e391f171968e972837d67e972717f2b85cbb92a1
Max Garceau <maxwellgarceau@gmail.com>
2018-05-28 12:12:54 -0500

    Added private routes.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
-/*jshint ignore: start*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import { startSetExpenses } from './actions/expenses';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
 // import filtersReducer from './reducers/filters';
 // import expensesReducer from './reducers/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 // import './playground/promises';
 
 const store = configureStore();
@@ -23,8 +22,29 @@ const jsx = (
   </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+    console.log('log in');
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
 });
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,15 +1,14 @@
-/*jshint ignore: start*/
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
-	<div>
-		<ExpensesSummary />	
-		<ExpenseListFilters />
-		<ExpenseList />
-	</div>
+  <div>
+    <ExpensesSummary />
+    <ExpenseListFilters />
+    <ExpenseList />
+  </div>
 );
 
 export default ExpenseDashboardPage;
